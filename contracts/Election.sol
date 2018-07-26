9680548dea1ac9f0862470b978e8ef84025447d4
Paul Fitzgerald <paulfitz99@gmail.com>
2018-07-26 11:14:32 +0100

    first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.4.24;
+
+contract Election {
+
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Read/write Candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    // Read/write candidate
+    string public candidate;
+
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    // Constructor
+    constructor() public {
+        addCandidate("Donald Trump");
+        addCandidate("Margaret Thatcher");
+    }
+
+    function addCandidate (string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+        
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
+    
+}
